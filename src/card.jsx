1452a9310b7be9ab7a1677dff64e0b5892ec177e
sagar1sharma <97111825+sagar1sharma@users.noreply.github.com>
2023-04-08 16:34:36 +0530

    Update card.jsx

--- src/card.jsx
+++ src/card.jsx
@@ -1,6 +1,4 @@
 import ln from './logoandname.png';
-import from from './Drop down.png';
-import to from './Drop down (1).png';
 
 function Card(props){
 
@@ -14,8 +12,6 @@ function Card(props){
     return(<div>
         <div className="card"></div>
             <img className='ln' src={ln}></img>
-            {/* <img className='from' src={from}></img>
-            <img className='to' src={to}></img> */}
             <form>
                 <input className="from" type="text" name="from"></input>
                 <input className="to" type="text" name="to"></input>
