244a22d2ac6177633481bcda5d86b57b0283f86f
sagar1sharma <sagarsharmazzz12@gmail.com>
2023-04-12 10:25:52 +0530

    Home Page Complete

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,17 +1,27 @@
 import Background from '../Background.jpg';
 import Header from '../Header';
-import weather from '../Temprature.png';
 import Card from '../card.jsx';
 import logo from '../path6.png';
 import line from '../Line 1.png';
+import axios from 'axios';
 import "../App.css";
+import {useState} from 'react';
 
 function Home(){
+    const day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const date = new Date();
+    const [weather, setWeather] = useState("");
+    axios.get('https://api.open-meteo.com/v1/forecast?latitude=28.64&longitude=77.22&hourly=temperature_2m&current_weather=true')
+    .then(res => setWeather(res.data.current_weather.temperature))
+    .catch(err => console.log(err));
+
+    console.log(weather);
+
     return(
         <div className="App">
      <img className = "background" src = {Background} alt = "this"></img>
-     <div className='dateAndTime'>March 26/2023  12:59 PM</div>
-     <img className = 'weather' src = {weather}></img>
+     <div className='dateAndTime'>{day[date.getDay()]} {date.getDate()}/{date.getMonth()}/{date.getFullYear()}  {date.getHours()}:{date.getMinutes()}</div>
+     <div className = 'weather dateAndTime'>{weather} °C <br></br> New Delhi</div>
      <Card />
      <img className='slogo' src={logo}></img>
      <img className='line1' src={line}></img>
@@ -20,6 +30,8 @@ function Home(){
      <img className='line4' src={line}></img>
      
      <Header />
+
+     <div className='footer'><p>Designed By Vishal<br></br>Developed by Abhay and Sagar</p></div>
     </div>
     );
 }
